9610dc040b2130a59b770bb5c1153de71df98ecb
Mayank Uchariya <mayankuchariya0@gmail.com>
2024-08-12 12:41:57 +0530

    initial commit

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,44 @@
+import getAllResults from "@/lib/getAllResults";
+import Item from "./components/Item";
+
+type Props = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+export async function generateMetaData({ params: { searchTerm } }: Props) {
+  const WikiData: Promise<SearchResult> = getAllResults(searchTerm);
+  const data = await WikiData;
+  const displayTerm = searchTerm.replaceAll("20%", " ");
+  if (data?.query?.pages) {
+    return {
+      title: `${displayTerm} not found`,
+    };
+  }
+
+  return {
+    title: `${displayTerm}`,
+    description: `Search results for ${displayTerm}`,
+  };
+}
+
+export default async function SearchResult({ params: { searchTerm } }: Props) {
+  const WikiData: Promise<SearchResult> = getAllResults(searchTerm);
+  const data = await WikiData;
+  const results: Result[] | undefined = data?.query?.pages;
+
+  const content = (
+    <main className="bg-white mx-auto max-w-screen-xl py-1 min-h-screen text-black">
+      {results ? (
+        Object.values(results).map((result) => {
+          return <Item key={result.pageid} result={result} />;
+        })
+      ) : (
+        <h2 className="p-2 text-xl font-bold">{`${searchTerm} not found`}</h2>
+      )}
+    </main>
+  );
+
+  return content;
+}
---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row fixed bottom-0 w-full drop-shadow-xl">
+        <h2>WikiRocket &copy;</h2>
+        <p>Email: mayankuchariya0@gmail.com</p>
+        <p>Contect Info : 8223943111</p>
+    </footer>
+  )
+}
